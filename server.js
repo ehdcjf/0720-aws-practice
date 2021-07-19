0baa5f31d15c3f415ec293eb697b7704b7a38e35
sleepppp <32008212+sleepppp@users.noreply.github.com>
2021-07-19 20:34:12 +0900

    update

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser')
 // npm install body-parser
 const app = express()
 const db = require('./models')
+const routes = require('./routes')
 
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false,}))
 
-db.sequelize.sync() // promise object
+db.sequelize.sync({focus:false}) // promise object
 .then(()=>{
     console.log('DB 접속 성공');
 })
@@ -14,49 +16,8 @@ db.sequelize.sync() // promise object
     console.log(`DB 접속 실패 : ${error} `)
 })
 
-app.get('/',(req,res)=>{
-    res.send('hello aws !')
-})
+app.use('/api',routes)
 
-app.post('/', async (req,res)=>{
-    // DB연결하고 
-    console.log('POSTMAN으로 실행함!');
-    res.json(req.body)
-    await User.create({
-        userid:req.body.userid,
-        username:req.body.username,
-        userpw:req.body.userpw
-    })
-})
-
-// 회원가입 url
-app.post('/user',(req,res)=>{
-
-})
-
-//회원정보 url
-app.get('/user',(req,res)=>{
-
-})
-
-//회원수정 url 
-app.patch('/user',(req,res)=>{
-
-})
-
-//회원탈퇴 url 
-app.delete('/user/',(req,res)=>{
-
-})
-
-// 로그인 
-// 게시판 view -> update 조회수 
-// 게시판 view -> patch?? view get ? 가져오기인데 ?
-// post get 
-
-app.delete('/user/follwer')
-app.delete('/user/folling')
-
-app.listen(3001,()=>{
-    console.log('start server port 3001')
+app.listen(3000,()=>{
+    console.log('start server port 3000')
 })
